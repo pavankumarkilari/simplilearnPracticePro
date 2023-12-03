7d125a0d3d18d9e9e85ad1b611bf2fe0d2225dc0
pavankumarkilari <pavankumarkilari519@gmail.com>
2023-12-03 19:49:27 +0530

    Changes have been committed.

---
+++ Phase1Section5.8/Phase1Section5.8/SubjectTeacher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Phase1Section5._8
+{
+    public class SubjectTeacher : Teacher
+    {
+        private Subject whichSubject;
+
+        public Subject WhichSubject
+        {
+            get { return whichSubject; }
+            set { whichSubject = value; }
+        }
+    }
+}
